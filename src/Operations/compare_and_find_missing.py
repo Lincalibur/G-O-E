45d0ab8c698c61059bcd671e512487fe0b43fba7
Lincalibur <135514909+Lincalibur@users.noreply.github.com>
2024-07-04 20:01:07 +0200

    Added descriptions of some other operations.

---
+++ src/Operations/compare_and_find_missing.py
@@ -0,0 +1,5 @@
+# Compare two excel files and find the values that are missing, take these lines and add them together in a new 
+# Excel sheet called Missing lines.
+# Also Add a Summary of how many lines were found in total.
+# How many entries were found 
+# How many Entries were not found.
---
+++ src/Operations/look_for_lines_meeting_criteria.py
@@ -0,0 +1,13 @@
+#Meet Criteria has the user taken to a new GUI, that will allow the user to fill in values in textBoxes
+#These will be next to eachother in the format:
+#Column name: ___________ looking for ____________
+#Button __Add__
+
+#When Button is pressed then the a new line/condition will be added to the list and the user will be able to add multiple conditions.
+#The Conditions will be listed below in a textbox for the user to see before they can select the "Save" button.
+#The Save Button will create a text file that will be deleted after the entire process is complete.
+#The saved text file will contain the Conditions and the conditions must be able to take (<>, >,<,=,>=,<=,|) as well as appropriate values.
+
+# The process will then take the Criteria on the Text file and make a new sheet for each criteria within one excel sheet.
+# The Process will then execute the Criteria as filters on the appropriate columns for each sheet.
+# Once the Entire process is complete everything is saved in one excel file called "applied_criteria"
